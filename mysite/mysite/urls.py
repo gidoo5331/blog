2a8dbe88b795341ceaddadc33d03bebc3cbf3d38
Gideon Bempong <gideonbempong533@gmail.com>
2023-07-27 14:12:28 +0000

    removed reract form view

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,12 +17,12 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import react
+# from .views import react
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r"^$", react),
-    re_path(r"^(?!admin)(?!api)(?!summernote)(?!auth)(?!api-auth)(?:.*)/?$", react),
+    # re_path(r"^$", react),
+    # re_path(r"^(?!admin)(?!api)(?!summernote)(?!auth)(?!api-auth)(?:.*)/?$", react),
 
     # Django summer note
     path('summernote/', include('django_summernote.urls')),
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+# django-environ
+import environ
+env = environ.Env()
+environ.Env.read_env()
+
+import pyrebase
+import os
 
 # React frontEnd Build View
 def react(request):
@@ -7,3 +14,21 @@ def react(request):
 # def react(request):
 #     context = { }
 #     return render(request, "index.html", context)
+
+
+# config = {
+#     "apiKey": "KEY",
+#     "authDomain": env('AUTH_DOMAIN'),
+#     "projectId": "project_name-id",
+#     "storageBucket": "project_name-id.appspot.com",
+#     "messagingSenderId": "msg_sender_id",
+#     "appId": "1:app_id",
+#     "measurementId": "G-measurement_id",
+#     "databaseURL": ""
+# }
+
+# firebase = pyrebase.initialize_app(config)
+# storage = firebase.storage()
+
+# storage = firebase.storage()
+# storage.child(PATH/DIRECTORY_ON_CLOUD).put(PATH_TO_LOCAL_IMAGE  )
